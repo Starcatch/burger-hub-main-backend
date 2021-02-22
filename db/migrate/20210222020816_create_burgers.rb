0e8fb34a91ce0f3c97ca751081bcc537724ffa4e
Starcatch <timurmishiev@gmail.com>
2021-02-21 21:11:09 -0500

    build a Burger model

---
+++ db/migrate/20210222020816_create_burgers.rb
@@ -0,0 +1,14 @@
+class CreateBurgers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :burgers do |t|
+      t.string :name
+      t.string :description
+      t.string :status
+      t.string :image
+      t.float :price
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
